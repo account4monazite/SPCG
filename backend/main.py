a62257f3d3ea77d6af9257502cf152bec1702972
account4monazite <shriyarane10@gmail.com>
2026-06-08 19:17:11 +0530

    idek atp dude

--- backend/main.py
+++ backend/main.py
@@ -6,13 +6,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
-orgins=[
-    "http://localhost:5173"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=orgins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
